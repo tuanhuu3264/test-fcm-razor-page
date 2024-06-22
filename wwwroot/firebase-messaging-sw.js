86cb6e6c88d418476d142f4a38cf093fa2409b23
tuanhuu3264 <tuanhuu3264@gmail.com>
2024-06-22 11:20:19 +0700

    test-fcm

---
+++ wwwroot/firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+﻿importScripts('https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.8/firebase-messaging.js');
+
+var firebaseConfig = {
+    // chua key
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/firebase-logo.png'
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
